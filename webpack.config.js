ac92ab5c15f668640800293f51ff779eaf12c149
Matt Preucil <mattpsbs@gmail.com>
2017-03-31 13:38:41 -0700

    Simplify webpack config.
    - Remove unnecessary env options.
     - We don’t need a streamless build.
     - Downstream consumers can compress.
    - This is a bit more consistent with our other projects,
      and seems like a bit of a more sane (simpler) distribution strategy.

--- webpack.config.js
+++ webpack.config.js
@@ -1,79 +1,33 @@
 "use strict";
 
-const webpack = require('webpack');
 const path = require('path');
 
-let libraryName = 'ApiAi';
-let libraryTarget = 'var';
-let outputFile = libraryName;
-let sourceMaps = true;
-let plugins = [];
-let entry = "_build.ts";
+module.exports = {
+  entry: {
+    'index': './ts/index.ts',
+  },
 
-module.exports = function(env) {
-  if (!env) {
-    env = {};
-  }
-  if (env && env.streamless) {
-    outputFile += '.streamless';
-    entry = "_build.streamless.ts"
-  }
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].js',
+    libraryTarget: 'commonjs-module',
+  },
 
-  // handle minification
-  if (env && env.compress) {
-    plugins.push(
-      new webpack.LoaderOptionsPlugin({
-        minimize: true,
-        debug: false
-      })
-    );
+  target: 'node',
 
-    plugins.push(
-      new webpack.optimize.UglifyJsPlugin({
-        compress: {
-          warnings: true,
-          keep_fnames: true
-        },
-        mangle: {
-          keep_fnames: true
-        }
-      })
-    );
-    outputFile += '.min';
-    sourceMaps = false;
-  } else {
-    // outputFile += libraryName;
-  }
+  devtool: 'source-map',
 
-  // handle custom target
-  if (env && env.target) {
-    libraryTarget = env.target;
-    outputFile += '.' + libraryTarget;
-  }
+  resolve: {
+    modules: [ 'node_modules' ],
+    extensions: [ '.ts' ],
+  },
 
-  outputFile += '.js';
-
-  return {
-    entry: [
-      path.join(__dirname, 'ts', entry)
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "awesome-typescript-loader",
+      },
     ],
-    devtool: sourceMaps ? 'source-map' : false,
-    output: {
-      path: path.join(__dirname, 'target'),
-      publicPath: "/target/",
-      filename: outputFile,
-      library: libraryName,
-      libraryTarget: libraryTarget
-    },
-
-    module: {
-      loaders: [
-        {test: /\.tsx?$/, loader: "awesome-typescript-loader"}
-      ]
-    },
-    resolve: {
-      extensions: ['.js', '.ts']
-    },
-    plugins: plugins
-  };
+  },
 };
