b3ab8daccf82cfccfbf40622d6e2bbc63530bcee
Matt Preucil <mattpsbs@gmail.com>
2017-04-03 09:23:39 -0700

    Fix indentation.

--- src/Request/Request.ts
+++ src/Request/Request.ts
@@ -6,24 +6,23 @@ import XhrRequest from '../XhrRequest';
 abstract class Request {
 
 	private static handleSuccess(xhr: XMLHttpRequest): Promise<IServerResponse> {
-	return Promise.resolve(JSON.parse(xhr.responseText));
+		return Promise.resolve(JSON.parse(xhr.responseText));
 	}
 
 	private static handleError(xhr: XMLHttpRequest): Promise<ApiAiRequestError> {
-
-	let error = new ApiAiRequestError(null);
-	try {
-		const serverResponse: IServerResponse = JSON.parse(xhr.responseText);
-		if (serverResponse.status && serverResponse.status.errorDetails) {
-		error = new ApiAiRequestError(serverResponse.status.errorDetails, serverResponse.status.code);
-		} else {
-		error = new ApiAiRequestError(xhr.statusText, xhr.status);
+		let error = new ApiAiRequestError(null);
+		try {
+			const serverResponse: IServerResponse = JSON.parse(xhr.responseText);
+			if (serverResponse.status && serverResponse.status.errorDetails) {
+				error = new ApiAiRequestError(serverResponse.status.errorDetails, serverResponse.status.code);
+			} else {
+				error = new ApiAiRequestError(xhr.statusText, xhr.status);
+			}
+		} catch (e) {
+			error = new ApiAiRequestError(xhr.statusText, xhr.status);
 		}
-	} catch (e) {
-		error = new ApiAiRequestError(xhr.statusText, xhr.status);
-	}
 
-	return Promise.reject<ApiAiRequestError>(error);
+		return Promise.reject<ApiAiRequestError>(error);
 	}
 
 	protected uri;
@@ -31,25 +30,21 @@ abstract class Request {
 	protected headers;
 
 	constructor(protected apiAiClient: ApiAiClient, protected options: IRequestOptions) {
+		this.uri = this.apiAiClient.getApiBaseUrl() + 'query?v=' + this.apiAiClient.getApiVersion();
+		this.requestMethod = XhrRequest.Method.POST;
+		this.headers = {
+			Authorization: 'Bearer ' + this.apiAiClient.getAccessToken(),
+		};
 
-	this.uri = this.apiAiClient.getApiBaseUrl() + 'query?v=' + this.apiAiClient.getApiVersion();
-	this.requestMethod = XhrRequest.Method.POST;
-	this.headers = {
-		Authorization: 'Bearer ' + this.apiAiClient.getAccessToken(),
-	};
-
-	this.options.lang = this.apiAiClient.getApiLang();
-	this.options.sessionId = this.apiAiClient.getSessionId();
-
+		this.options.lang = this.apiAiClient.getApiLang();
+		this.options.sessionId = this.apiAiClient.getSessionId();
 	}
 
 	public perform(overrideOptions = null): Promise<IServerResponse> {
-
-	const options = overrideOptions ? overrideOptions : this.options;
-
-	return XhrRequest.ajax(this.requestMethod, this.uri, options as IStringMap, this.headers)
-		.then(Request.handleSuccess.bind(this))
-		.catch(Request.handleError.bind(this));
+		const options = overrideOptions ? overrideOptions : this.options;
+		return XhrRequest.ajax(this.requestMethod, this.uri, options as IStringMap, this.headers)
+			.then(Request.handleSuccess.bind(this))
+			.catch(Request.handleError.bind(this));
 	}
 }
 
